fbee6188815eb15f465a61cdd0c9a274ff1b8ae5
George Siaskov <egor.record@yandex.ru>
2020-02-13 01:33:20 +0300

    Connected to MySQL; Sort JSON from Parser; Safe Team first and team second;

--- src/main/java/ru/sportbetsbattle/games/GamesController.java
+++ src/main/java/ru/sportbetsbattle/games/GamesController.java
@@ -1,27 +1,60 @@
 package ru.sportbetsbattle.games;
 
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/parse")
 public class GamesController {
 
-    GamesRepository gamesRepository;
 
-    @GetMapping("/football")
-    public String getAllMatches() {
-        return "OK";
+    private GamesRepository gamesRepository;
+
+    @Autowired
+    public GamesController(GamesRepository gamesRepository) {
+        this.gamesRepository = gamesRepository;
     }
 
     @PostMapping("/update")
-    public HttpStatus updateGame(@RequestBody String jsonStr) {
-        System.out.println(jsonStr);
-        return HttpStatus.OK;
+    public HttpStatus updateGame(@RequestBody String jsonStr) throws ParseException {
 
+        JSONArray weather = handleJSON(jsonStr);
+
+        System.out.println(weather);
+
+        for (Object o : weather) {
+            JSONObject jsonObj = (JSONObject) o;
+            System.out.println(jsonObj.get("team_first"));
+            System.out.println(jsonObj.get("team_second"));
+            System.out.println(jsonObj.get("score_first"));
+            System.out.println(jsonObj.get("score_second"));
+
+            Games game = new Games("Football", "Чемпионат Испании. Примера", (String) jsonObj.get("team_first"), (String) jsonObj.get("team_second"));
+            gamesRepository.save(game);
+        }
+
+        gamesRepository.findFirstByTeamFirst("Барселона").forEach(bauer -> {
+            System.out.println(bauer.getTeamSecond());
+        });
+
+        return HttpStatus.OK;
+    }
+
+    private JSONArray handleJSON(String json) throws ParseException {
+        JSONParser parser = new JSONParser();
+        Object object = parser.parse(json);
+        return (JSONArray) object;
     }
 
 }
--- src/main/java/ru/sportbetsbattle/games/GamesRepository.java
+++ src/main/java/ru/sportbetsbattle/games/GamesRepository.java
@@ -1,14 +1,26 @@
 package ru.sportbetsbattle.games;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
+import java.util.List;
+
 public interface GamesRepository extends PagingAndSortingRepository<Games, Long> {
 
     Page<Games> findByNameOfTheGame(@Param("nameOfTheGame") String nameOfTheGame, Pageable page);
     Page<Games> findByLeague(@Param("league") String league, Pageable page);
+    Page<Games> findFirstByLastUpdatedAndTeamFirst(@Param("LastUpdated") Date date, @Param("teamFirst") String name, Pageable page);
+
+    Games findById(long id);
+
+    List<Games> findFirstByTeamFirst(String teamFirst);
+
 
 }
